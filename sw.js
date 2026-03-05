3999e5b22f4e685cae651f49ce3031b96af90bf9
dhooja-source <dhooja@gmail.com>
2026-03-06 01:47:02 +0200

    Implement service worker caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('fetch', function(event) {});
+// v2 - clear all caches
+var CACHE = ‘shifty-v2’;
+
+self.addEventListener(‘install’, function(e) {
+self.skipWaiting();
+});
+
+self.addEventListener(‘activate’, function(e) {
+e.waitUntil(
+caches.keys().then(function(keys) {
+return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+}).then(function() { return self.clients.claim(); })
+);
+});
+
+self.addEventListener(‘fetch’, function(event) {
+event.respondWith(fetch(event.request));
+});
